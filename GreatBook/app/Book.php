7a66114cf38eb5d5381dc2f9420020d9c867a652
An <anbn247@gmail.com>
2019-12-15 17:16:05 +0700

    Complete book model

--- GreatBook/app/Book.php
+++ GreatBook/app/Book.php
@@ -4,7 +4,16 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Book extends Model
+class Project extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function path()
+    {
+        return "/projects/{$this->id}";
+    }
+    public function owner()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
