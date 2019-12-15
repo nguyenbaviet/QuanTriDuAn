42ead14472493aa821a7e84320bfa74dfa3dde0f
An <anbn247@gmail.com>
2019-12-15 17:12:37 +0700

    CRUD book

--- GreatBook/app/Http/Controllers/BooksController.php
+++ GreatBook/app/Http/Controllers/BooksController.php
@@ -4,7 +4,39 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class BooksController extends Controller
+use App\Project;
+
+class ProjectsController extends Controller
 {
-    //
+    public function index()
+    {
+        $projects = auth()->user()->projects;
+        return view('projects.index', compact('projects'));
+    }
+
+    public function show(Project $project)
+    {
+        if (auth()->user()->isNot($project->owner)) {
+            abort(403);
+        }
+        return view('projects.show', compact('project'));
+    }
+
+    public function create()
+    {
+        return view('projects.create');
+    }
+
+    public function store()
+    {
+        // validate
+        $attributes = request()->validate([
+            'title' => 'required',
+            'description' => 'required',
+        ]);
+
+        auth()->user()->projects()->create($attributes);
+
+        return redirect('/projects');
+    }
 }
